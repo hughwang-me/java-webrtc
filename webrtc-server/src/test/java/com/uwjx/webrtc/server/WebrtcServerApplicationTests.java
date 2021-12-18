ea3cfa8d25a853dcb45c023522763b401f2e346a
wanghuan <18501667737@163.com>
2021-12-18 22:38:00 +0800

    init

---
+++ webrtc-server/src/test/java/com/uwjx/webrtc/server/WebrtcServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uwjx.webrtc.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebrtcServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
